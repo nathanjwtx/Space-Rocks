df7ff613054e5a41465a9f70539157dd4d7517e2
Nathan <nathanjwtx@nathanjwtx.io>
2019-01-16 06:24:17 -0600

    Game over page restarts game on button press. Needs an exit routine

--- scenes/game_over/GameOverPage.cs
+++ scenes/game_over/GameOverPage.cs
@@ -3,21 +3,17 @@ using System;
 
 public class GameOverPage : CanvasLayer
 {
-    // Member variables here, example:
-    // private int a = 2;
-    // private string b = "textvar";
 
     public override void _Ready()
     {
-        // Called every time the node is added to the scene.
-        // Initialization here
         GetNode<Label>("MarginContainer/GameOver").Text = "It's Game Over man! It's Game Over!";
     }
 
-//    public override void _Process(float delta)
-//    {
-//        // Called every frame. Delta is time since last frame.
-//        // Update game logic here.
-//        
-//    }
+    private void _on_Restart_pressed()
+    {
+        GetTree().ChangeScene("res://scenes/main/Main.tscn");
+    }
 }
+
+
+
